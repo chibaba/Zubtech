efaeee8a5af0b5cdd45a6a27c6dd7f9273ef5178
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-22 15:56:17 +0100

    wip

--- src/main/java/com/chibabaCodes/ecommerce/entity/Product.java
+++ src/main/java/com/chibabaCodes/ecommerce/entity/Product.java
@@ -1,4 +1,35 @@
 package com.chibabaCodes.ecommerce.entity;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
 public class Product {
+
+    private Long id;
+
+    private String sku;
+
+    private String  name;
+
+    private  String description;
+
+    private BigDecimal unitPrice;
+
+    private String imageUrl;
+
+    private  boolean acive;
+
+    private int  unitsInStock;
+
+    private Date  dateCreated;
+
+    private Date lastCreated;
 }
