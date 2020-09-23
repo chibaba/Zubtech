b22e13f27245f416e31d40dfc274370301bbe268
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-23 14:33:37 +0100

    wip

--- src/main/java/com/chibabaCodes/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/chibabaCodes/ecommerce/config/MyDataRestConfig.java
@@ -1,8 +1,24 @@
 package com.chibabaCodes.ecommerce.config;
 
+import com.chibabaCodes.ecommerce.entity.Product;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods  for produt: PUT, POST, DELETE
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
 }
